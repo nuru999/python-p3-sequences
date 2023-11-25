8812d06df387e85ed9c99fe081341bbc202583cc
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-25 11:02:55 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+ 
+ if length <= 0:
+    print([])
+ else: 
+    fib_sequence = [0, 1]
+    if length == 1:
+        print ([0])
+    elif length == 2:
+       print(fib_sequence)
+    else:
+       for _ in range(2, length):
+          next_number = fib_sequence[-1] + fib_sequence [-2]
+          fib_sequence.append(next_number)
+          print(fib_sequence)
